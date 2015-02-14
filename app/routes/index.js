f62aeac361c21869d637eb502e38cf686809c8d2
chrisdevwords <chrisedwards82@ChrisEdwardss-MacBook-Pro.local>
2015-02-14 10:27:08 -0500

    bootstrapping 1st page of yotube data results

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,18 +3,27 @@
 var express = require('express');
 var router  = express.Router();
 var Imgur = require('../lib/Imgur');
+var Youtube = require('youtube-node');
+
+var IMGUR_QUERY = 'dunk+ext%3Agif';
+var YOU_TUBE_QUERY = 'slam+dunk+basketball';
 
 /* GET home page */
 router.get('/', function(req, res){
     var imgurData;
-    var imgur = new Imgur(process.env.IMGUR_KEY || req.param('imgur_key') || '');
-    imgur.search('dunk+ext%3Agif', 'top', 0)
+    var imgur = new Imgur(process.env.IMGUR_KEY || '');
+    var youtube = new Youtube();
+    imgur.search(IMGUR_QUERY, 'top', 0)
         .always( function (resp) {
             imgurData = resp.data;
-            res.render('index', {
-                title : '247 Dunks!',
-                imgur : imgurData,
-                serverVars : true
+            youtube.setKey(process.env.YOUTUBE_KEY || '');
+            youtube.search(YOU_TUBE_QUERY, 50, function(resultData) {
+                res.render('index', {
+                    title : '247 Dunks!',
+                    imgur : imgurData,
+                    youtube : resultData,
+                    serverVars : true
+                });
             });
         });
 });
